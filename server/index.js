d3adac7cfa2ee99510996b10bd059b9aee850293
Francisco Carmona <flcarmonac@yahoo.com>
2023-10-30 21:58:15 -0300

    update

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import { Server } from "socket.io"
 import { createServer } from "node:http"
 
 dotenv.config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 const app = express()
 const server = createServer(app)
